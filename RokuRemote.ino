a71f1de9d7acb04d54c1786d7a3bb090a828a390
Jon <jon.bailey45@gmail.com>
2014-03-29 01:40:40 -0400

    Works! As well as some function codes determined

--- RokuRemote.ino
+++ RokuRemote.ino
@@ -3,12 +3,10 @@
 const int ledPin =  9;      // the number of the LED pin
 #define PULSE_ON_PWM     (TCCR1A |= _BV(COM1A1))
 #define PULSE_OFF_PWD    (TCCR1A &= ~(_BV(COM1A1)))
-#define PULSE_ON  ledState=HIGH
-#define PULSE_OFF ledState=LOW
+#define PULSE_ON         ledState=HIGH
+#define PULSE_OFF        ledState=LOW
 
 int ledState = LOW;             // ledState used to set the LED
-long previousMillis = 0;        // will store last time LED was updated
-unsigned long interval = 1000;           // interval at which to blink (milliseconds)
 
 #define INIT_MARK   9000
 #define INIT_SPACE  4500
@@ -48,7 +46,6 @@ void txNEC(unsigned char dev, unsigned char subdev, unsigned char command) {
             SEND_ZERO;
         }
     }
-    
     //the 8-bit command
     for (int i = 0; i < 8; i++ ){
         if((command >> i) & 0x1) {
@@ -57,7 +54,6 @@ void txNEC(unsigned char dev, unsigned char subdev, unsigned char command) {
             SEND_ZERO;
         }
     }
-    
     //the 8-bit logical inverse of the command
     for (int i = 0; i < 8; i++ ){
         if(((~command) >> i) & 0x1) {
@@ -66,14 +62,13 @@ void txNEC(unsigned char dev, unsigned char subdev, unsigned char command) {
             SEND_ZERO;
         }
     }
-    
     //a final 562.5µs pulse burst to signify the end of message transmission.
     PULSE_ON; delayMicroseconds(END_MARK);
     PULSE_OFF;delay(40);
 }
 
 void txRepNEC1(unsigned char dev, unsigned char subdev, unsigned char command, unsigned int delta) {
-    txNEC(dev,subdev,command); //-78 delay = 40ms embedded in tx
+    txNEC(dev,subdev,command); //(-78 delay = 40ms) embedded in tx
     
     unsigned long goal = millis() + delta;
     //unsigned long current = millis();
@@ -91,23 +86,73 @@ unsigned char cnt;
 void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  initCarrierPulse();
+  init38kPulse();
   cnt = 0;
 }
 
-
-
 void loop()
 {
-  /*txRepNEC1(234, 194, 0xBF, 3000); //0xBF - power, 0xBB menu/home
-  delay(1000);
-  txRepNEC1(234, 194, 0xC9, 3000); //0xBF - power, 0xBB menu/home
-  delay(1000);
-  txRepNEC1(234, 194, 0xBB, 3000); //0xBF - power, 0xBB menu/home
-  delay(2000);*/
   txNEC(234,194,cnt++);
 }
 
+// 38khz = 26.3us
+// Although probably want  up down beat up down beat and not up beat down beat
+// Where beat is the 38 khz metronome
+void init38kPulse(void) {
+    cli();
+    const uint16_t ticks = 16000000/ 38000 / 2;
+    TCCR1A = 0x00;         // Clear timer1's control registers
+    TCCR1B = 0x00;
+    TCCR1C = 0x00;
+    
+    TCNT1 = 0;             // Pre-load the timer to 0
+    OCR1A = ticks-1;           // Set output compare register to 210. for 38khz
+                           // Was originally 421, but divide by 2, because i think I want
+                           // Up and then down, to happen each beat of the frequency
+                           // Do i want/need to subtract 1 as well?
+                  
+    TCCR1B |= _BV(WGM12);  // Turn on CTC mode
+    TCCR1B |= _BV(CS10);  // Set prescaler to 1
+    TIMSK1 |= _BV(OCIE1A); // Enable timer compare interrupt 
+    sei();
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+    // Is there proper PWM thing on timer i should be using
+    if (ledState == HIGH) {
+        PORTB ^= (1<<1);
+    } else {
+        PORTB &= ~(1<<1);
+    }
+}
+
+/* Experimentally Determined function codes so far
+3           home
+25          up
+51          down
+173         right
+42          ok
+180         <<
+179         some form of down-ish
+203/75      netflix
+97/225      asterisk/options
+144/16      amazon
+210/82      pandora
+160         play/pause ?
+153         some uppish, maybe rewind?
+230         back/actual back
+102         Another form of back?
+131         back3/home?
+248         ?
+8           vudu
+120         ? 
+127         crackle
+132         ?
+136         play?
+143         movies
+*/
+    
 /*void init38kPulse(void) {
     cli();
     const uint16_t ticks = 16000000 / 38222 / 2; // Should be about 210
@@ -125,33 +170,3 @@ void loop()
     sei();
 }*/
 
-// 38khz = 26.3us
-// Although probably want  up down beat up down beat and not up beat down beat
-// Where beat is the 38 khz metronome
-void initCarrierPulse(void) {
-    cli();
-    const uint16_t ticks = 16000000/ 38000 / 2;
-    TCCR1A = 0x00;         // Clear timer1's control registers
-    TCCR1B = 0x00;
-    TCCR1C = 0x00;
-    
-    TCNT1 = 0;             // Pre-load the timer to 0
-    OCR1A = ticks;           // Set output compare register to 210. for 38khz
-                           // Was originally 421, but divide by 2, because i think I want
-                           // Up and then down, to happen each beat of the frequency
-                           // Do i need to subtract 1 as well?
-                  
-    TCCR1B |= _BV(WGM12);  // Turn on CTC mode
-    TCCR1B |= _BV(CS10);  // Set prescaler to 1
-    TIMSK1 |= _BV(OCIE1A); // Enable timer compare interrupt 
-    sei();
-}
-
-ISR(TIMER1_COMPA_vect)
-{
-    // Is there proper PWM thing on timer i should be using
-    if (ledState == HIGH) {
-        PORTB ^= (1<<1);
-    }
-    // yeah, letus use OC1 == PB1 == Pin 9 instead
-}
